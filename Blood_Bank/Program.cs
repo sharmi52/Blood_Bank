84ea9f2716887d940953c5b8f97d37b4ff01f655
sharmi52 <88834305+sharmi52@users.noreply.github.com>
2021-09-12 23:55:27 +0600

    BB Update 7
    Design of Login Form
    
    Co-Authored-By: Ridwan51 <88870729+Ridwan51@users.noreply.github.com>

--- Blood_Bank/Program.cs
+++ Blood_Bank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blood_Bank.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Blood_Bank
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmhome());
+            Application.Run(new frmLogin());
         }
     }
 }
